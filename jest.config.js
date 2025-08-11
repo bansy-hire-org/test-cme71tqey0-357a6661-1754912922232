acee12b722e8030472a225ad65d57168802113bc
Bansy Assessment Platform <noreply@bansy.com>
2025-08-11 11:48:54 +0000

    Add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// jest.config.js
+module.exports = {
+    testEnvironment: 'node',
+    testMatch: ['<rootDir>/server/**/*.test.js'],
+    verbose: true,
+    forceExit: true,
+    clearMocks: true
+  };
